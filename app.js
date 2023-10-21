84182ac413050a92e58879ce974e54e9fc95ebfa
Paul <fpaul847@gmail.com>
2023-10-21 16:19:40 +0100

    commit funct subtract

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ function addNum (a,b) {
 
 let result = addNum(45,12)
 console.log(result)
+
+function subtract (c,d) {
+    return c - d;
+    function divide (r,t) {
+
+        let result1 = r / t;
+        return result1
+    }
+    
+    return divide
+}
+
+let mySubtract = subtract(123,23);
+console.log(mySubtract);
